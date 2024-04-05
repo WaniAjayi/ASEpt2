65ec2866cf78ca958201038edf6883c34dcceddb
WaniAjayi <S.Ajayi9355@student.leedsbeckett.ac.uk>
2024-04-05 20:10:19 +0100

    Other Resources

---
+++ WinFormsApp2/ClearCanvas.cs
@@ -0,0 +1,19 @@
+﻿namespace WinFormsApp2
+{
+    public class ClearCanvas
+    {
+        private readonly PictureBox pictureBox1;
+
+        public ClearCanvas(PictureBox pictureBox)
+        {
+            this.pictureBox1 = pictureBox;
+        }
+
+
+        public void Execute(Color backgroundColor)
+        {
+            pictureBox1.Image = null; 
+            pictureBox1.BackColor = backgroundColor;
+        }
+    }
+}
