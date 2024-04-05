a52b8dde9c0c4d2bd52168d2d78425f2c1585902
WaniAjayi <S.Ajayi9355@student.leedsbeckett.ac.uk>
2024-04-05 20:06:30 +0100

    Inheritance implementation

---
+++ WinFormsApp2/ICommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp2
+{
+    public interface ICommand
+    {
+        /// <summary>
+        /// Executes the command with the given parameters.
+        /// </summary>
+        /// <param name="parameters">An array of objects representing the parameters needed for execution.</param>
+        void Execute(object[] parameters);
+    }
+}
